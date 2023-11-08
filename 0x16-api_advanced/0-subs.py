21fc38eca71c72b1ae274a4397084b636a596b67
Tevin Murimi Mwenda <tevinmurimimwenda@gmail.com>
2023-11-07 17:51:00 -0800

    0. How many subs?

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,29 @@
+import requests
+
+def number_of_subscribers(subreddit):
+    """
+    Retrieves the number of subscribers for a given subreddit using the Reddit API.
+    
+    Args:
+        subreddit (str): The name of the subreddit.
+        
+    Returns:
+        int: The number of subscribers for the subreddit.
+             Returns 0 if the subreddit is invalid.
+             
+    Raises:
+        Exception: If the request to the API fails.
+    """
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {"User-Agent": "MyAPIAgent"}  # Set a custom User-Agent header
+
+    response = requests.get(url, headers=headers)
+    
+    if response.status_code == 200:
+        data = response.json()
+        return data["data"]["subscribers"]
+    elif response.status_code == 404:
+        return 0  # Invalid subreddit
+    else:
+        raise Exception(f"Request failed with status code {response.status_code}")
+

16b85bbbd8b5cbd48ff792116905ec8ba93e9b88
Tevin Murimi Mwenda <tevinmurimimwenda@gmail.com>
2023-11-07 18:33:56 -0800

    2. Recurse it!

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -4,41 +4,35 @@
 
 import requests
 
+import requests
 
-def recurse(subreddit, hot_list=[]):
-    """
-    Recursively retrieves the titles of all hot articles
-    for a given subreddit using the Reddit API.
 
-    Args:
-        subreddit (str): The name of the subreddit.
-        hot_list (list): A list to store the titles of
-        hot articles (default is an empty list).
-
-    Returns:
-        list: A list containing the titles of all hot articles.
-        If no results are found, it returns None.
-
-    """
+def recurse(subreddit, hot_list=[], after=None):
     url = f"https://www.reddit.com/r/{subreddit}/hot.json"
     headers = {"User-Agent": "MyAPIAgent"}  # Set a custom User-Agent header
+    params = {"after": after} if after else None
+    myRequest = requests.get(
+            url,
+            headers=headers,
+            params=params,
+            allow_redirects=False)
 
-    response = requests.get(url, headers=headers, allow_redirects=False)
+    if myRequest.status_code == 200:
+        try:
+            data = myRequest.json()
+            posts = data["data"]["children"]
 
-    if response.status_code == 200:
-        data = response.json()
-        posts = data["data"]["children"]
+            for post in posts:
+                title = post["data"]["title"]
+                hot_list.append(title)
 
-        for post in posts:
-            title = post["data"]["title"]
-            hot_list.append(title)
-
-        # Check if there is a next page
-        after = data["data"]["after"]
-        if after:
-            return recurse(subreddit, hot_list, after)
-        else:
-            return hot_list
-
+            after = data["data"]["after"]
+            if after:
+                return recurse(subreddit, hot_list, after)
+            else:
+                return hot_list
+        except requests.exceptions.RequestException as e:
+            print(f"An error occurred: {e}")
+            return None
     else:
-        return None  # Invalid subreddit or request failed
+        return None
